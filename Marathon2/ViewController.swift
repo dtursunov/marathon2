2762041a17a368eb39caf9090da7777eddacdc6a
Tursunov Diyor <diyor.9408@gmail.com>
2024-11-05 20:30:37 +0500

    init

---
+++ Marathon2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon2
+//
+//  Created by Diyor Tursunov on 05/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
